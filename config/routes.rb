dda5b6b01490dccf1b06a3ea6c8e5caff504b1da
brazhnyknorman <brazhnyknorman@gmail.com>
2025-09-19 16:22:35 -0400

    Implement basic Devise auth & User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # Routing the controller actions for User I created
+  get "users/index"
+  get "users/show"
+
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +16,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "events#index"
+
+  resources :events, only: [ :index ]
+  resources :users, only: [ :index, :show ]
 end
